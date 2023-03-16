dfc04d1d5896f86eed601787422973ba0ee8ae53
Nafiseh <72595130+nafisehOfoghi@users.noreply.github.com>
2023-03-16 12:30:39 +0100

    Update FeauturedCategories.js

--- src/components/FeauturedCategories.js
+++ src/components/FeauturedCategories.js
@@ -4,29 +4,28 @@ function FeaturedCategories() {
   return (
     <div className="featured-cat-list">
       <div className="first-col">
-        <p className="title"> می خواهید پوستی کاملاً سالم داشته باشید؟</p>
-        <p className="sub-title"> خرید کنید</p>
+        <p className="title"> Do you want to have perfectly healthy skin?</p>
+        <p className="sub-title"> Buy Here</p>
       </div>
 
       <div className="second-col">
         <div className="second-col section1">
-          <p className="title"> رز و درخشش</p>
-          <p className="sub-title"> خرید کنید</p>
+          <p className="title"> Rose and shine</p>
+          <p className="sub-title">Buy Here</p>
         </div>
 
         <div className="second-col section2">
-          <p className="title">ناب ترین رایحه ها</p>
-          <p className="sub-title"> خرید کنید</p>
+          <p className="title">The purest scents</p>
+          <p className="sub-title"> Buy Here</p>
         </div>
       </div>
 
       <div className="third-col">
-        <p className="title"> با طعم ویتامین سی</p>
+        <p className="title"> With vitamin C</p>
         <p className="description">
-          ساخته شده با آنتی اکسیدان های طبیعی، لوسیون های ما پوستی شفاف و سالم
-          را برایتان به ارمغان می آورند{" "}
+        Made with natural antioxidants, our lotions leave skin clear and healthy
         </p>
-        <p className="sub-title"> خرید کنید</p>
+        <p className="sub-title"> Buy Here</p>
       </div>
     </div>
   );
