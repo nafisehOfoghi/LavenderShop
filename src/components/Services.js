e8d9fbd24b1f80d29506d01d949f3147af488670
Nafiseh <72595130+nafisehOfoghi@users.noreply.github.com>
2023-03-16 12:36:38 +0100

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,13 +8,13 @@ function Services(props)
 
         <div className="services">
 
-            <Service className="service1" title="ارسال رایگان" subtitle="تا نهایت 2 روز کاری" icon="pi pi-car"/>
+            <Service className="service1" title="Free Delivery" subtitle="Up to 2 working days" icon="pi pi-car"/>
 
-            <Service className="service2" title="بیش از 80 برند" subtitle="به روزرسانی روزانه" icon="pi pi-star"/>
+            <Service className="service2" title="More than 80 brands" subtitle="Daily update" icon="pi pi-star"/>
 
-            <Service className="service3" title="ارسال به تمام نقاط دنیا" subtitle="اطلاعات بیشتر را ببینید" icon="pi pi-globe"/>
+            <Service className="service3" title="World Wide Shipping" subtitle="More Information" icon="pi pi-globe"/>
 
-            <Service className="service4" title="پشتیبانی 24 ساعته" subtitle="مشاوره رایگان با تیم ما" icon="pi pi-phone"/>
+            <Service className="service4" title="24 hour support" subtitle="Free consultation" icon="pi pi-phone"/>
 
 
         </div>
