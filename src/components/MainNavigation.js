9091b67837638436ff9bbdbe4eca30c042ac640c
Nafiseh <72595130+nafisehOfoghi@users.noreply.github.com>
2023-03-16 12:33:40 +0100

    Update MainNavigation.js

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -11,27 +11,28 @@ import Register from "./Register";
 function MainNavigation() {
   const [login, setLogin] = useState(false);
   const [signup, setSignup] = useState(false);
+  const [desktop, setDesktop] = useState(window.matchMedia("(min-width: 768px)").matches);
 
   return (
     <header className="header">
       <ul className="login">
         <li>
           <Button
-            label="ورود"
+            label="Login"
             className="login-button"
             onClick={() => setLogin(true)}
           />{" "}
           &nbsp;
           <Dialog
-            header="ورود به سایت"
+            header="Login Form"
             visible={login}
-            style={{ width: "40vw" }}
+            style={{ width : !desktop ? "80%" : "50%" }}
             onHide={() => setLogin(false)}
           >
             <div>
               <ul className="add_product_field">
                 <li className="label">
-                  <label htmlFor="username">نام کاربری</label>
+                  <label htmlFor="username">Username</label>
                 </li>
                 <li>
                   {" "}
@@ -42,7 +43,7 @@ function MainNavigation() {
               <div style={{ marginTop: "2rem" }}>
                 <ul className="add_product_field">
                   <li className="label">
-                    <label htmlFor="password">رمز عبور</label>
+                    <label htmlFor="password">Password</label>
                   </li>
                   <li>
                     {" "}
@@ -61,7 +62,7 @@ function MainNavigation() {
               >
                 <Link to="/dashboard">
                   <Button
-                    label="ورود"
+                    label="Login"
                     onClick={() => {
                       setLogin(false);
                     }}
@@ -73,14 +74,14 @@ function MainNavigation() {
         </li>
         <li>
           <Button
-            label="ثبت نام"
+            label="Register"
             className="register-button"
             onClick={() => setSignup(true)}
           />
           <Dialog
-            header="فرم ثبت نام"
+            header="Register Form"
             visible={signup}
-            style={{ width: "50vw" }}
+            style={{ width : !desktop ? "80%" : "50%" }}
             onHide={() => setSignup(false)}
           >
             <Register />
@@ -104,14 +105,15 @@ function MainNavigation() {
         <div className="logo">
         <Link to="/">
           <ul>
+          <li>
+            <p className="logo-text">
+            Lavendar Shop
+            </p>
+            </li>
             <li className="logo-image" >
             <img src={ require('../logo_fin.png') } />
             </li>
-            <li>
-            <p>
-            فروشگاه زیبا
-            </p>
-            </li>
+            
             </ul>
             </Link>
             </div>
